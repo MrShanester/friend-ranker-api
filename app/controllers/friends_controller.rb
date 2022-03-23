fe17bb645ec600ab3a6e4b7d660f516423205a24
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-23 11:19:58 -0700

    Adds CRUD to friend controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,44 @@
 class FriendsController < ApplicationController
+  def index
+    friends = Friend.all
+    render json: friend.as_json
+  end
+
+  def show
+    friend = Friend.find_by id: params[:id]
+    render json: friend.as_json
+  end
+
+  def create
+    friend = Friend.new(
+      name: params[:name],
+      details: params[:details],
+      points: params[:points],
+      image_url: params[:image_url]
+    )
+    if friend.save
+      render json: friend.as_json
+    else
+      render json: {error: friend.errors.full_messages}
+    end
+  end
+
+  def update
+    friend = Friend.find_by id: params[:id]
+    friend.name = params[:name] || friend.name
+    friend.details = params[:details] || friend.details
+    friend.points = params[:points] || friend.points
+    friend.image_url = params[:image_url] || friend.image_url
+    if friend.save
+      render json: friend.as_json
+    else
+      render json: {error: friend.errors.full_messages}
+    end
+  end
+
+  def destroy
+    friend = Friend.find_by id: params[:id]
+    friend.delete
+    render json: {message: "Friend deleted"}
+  end
 end
